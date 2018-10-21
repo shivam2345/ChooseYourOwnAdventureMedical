1cf4cc369f4e14355e3888c29b80245258fb88d2
mgalan1 <44332488+mgalan1@users.noreply.github.com>
2018-10-21 14:00:21 -0700

    question pages

--- game.js
+++ game.js
@@ -1,10 +1,59 @@
 function reply_click(clicked_id)
 {
-    if (clicked_id == "1")
+    if (clicked_id === "A")
     {
       window.open("correctAnswer1.html", "_self");
     }
     else {
       window.open("tryAgain.html", "_self");
     }
+}
+
+function question2(clicked_id)
+{
+    if (clicked_id === "C")
+    {
+      window.open("correctAnswer2.html", "_self");
+    }
+    else {
+      window.open("tryAgain.html", "_self");
+    }
+}
+
+function question3(clicked_id)
+{
+    if (clicked_id === "B")
+    {
+      window.open("correctAnswer3.html", "_self");
+    }
+    else {
+      window.open("tryAgain.html", "_self");
+    }
+}
+
+function question4(clicked_id)
+{
+    if (clicked_id === "D")
+    {
+      window.open("correctAnswer4.html", "_self");
+    }
+    else {
+      window.open("tryAgain.html", "_self");
+    }
+}
+
+function question5(clicked_id)
+{
+    if (clicked_id === "A")
+    {
+      window.open("correctAnswer5.html", "_self");
+    }
+    else {
+      window.open("tryAgain.html", "_self");
+    }
+}
+
+function question6(clicked_id)
+{
+     window.open("correctAnswer6.html", "_self");
 }
