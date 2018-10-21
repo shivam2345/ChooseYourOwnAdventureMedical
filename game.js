c4e1434a04007cda48f7b9f792c755e79515d85b
shivam2345 <32351363+shivam2345@users.noreply.github.com>
2018-10-21 11:38:04 -0700

    game blueprint

---
+++ game.js
@@ -0,0 +1,10 @@
+function reply_click(clicked_id)
+{
+    if (clicked_id == "1")
+    {
+      window.open("correctAnswer1.html", "_self");
+    }
+    else {
+      window.open("tryAgain.html", "_self");
+    }
+}
